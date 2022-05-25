5bef481b45663358dc8b6e3fd2f16ace894d7ad1
hannaagerlo <Hannaagerlo@gmail.com>
2022-05-25 14:17:52 +0200

    Added more logic and view models

--- Courses-Api/Controllers/CourseController.cs
+++ Courses-Api/Controllers/CourseController.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Courses_Api.Data;
+using Courses_Api.Models;
+using Courses_Api.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Courses_Api.Controllers
 {
@@ -10,6 +14,56 @@ namespace Courses_Api.Controllers
     [Route("api/v1/courses")]
     public class CourseController : ControllerBase
     {
+        private readonly CourseContext _context;
+        public CourseController(CourseContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        [HttpGet]
+        public async Task<ActionResult<List<CourseViewModel>>> ListCourses()
+        {
+            var response = await _context.Courses.ToListAsync(); 
+            var courseList = new List<CourseViewModel>();
+
+            foreach(var course in response)
+            {
+                courseList.Add(
+                    new CourseViewModel{
+                        CourseId = course.Id,
+                        CourseNumber = course.CourseNumber,
+                        Title = course.Title,
+                        Lenght = course.Lenght,
+                        Category = course.Category,
+                        Description = course.Description,
+                        Details = course.Details
+                    }
+                );
+            }
+            return Ok(courseList);
+        }
+        //POST
+        [HttpPost]
+        public async Task<ActionResult<Course>> AddCourse(PostCoursesViewModel course)
+        {
+            
+            var courseToAdd = new Course
+            {
+                CourseNumber = course.CourseNumber,
+                Title = course.Title,
+                Lenght = course.Lenght,
+                Category = course.Category,
+                Description = course.Description,
+                Details = course.Details
+            };
+            await _context.Courses.AddAsync(courseToAdd);
+            await _context.SaveChangesAsync();
+            return StatusCode(201, course);
         
+        }
+        
+        // PUT
+        //DELETE
     }
 }
